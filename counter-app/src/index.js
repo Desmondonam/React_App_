d933368c39bb9593b8a969d6f31954516773ec6e
Desmondonam <desmondonam@gmail.com>
2022-10-18 12:22:42 +0300

    change index

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import registerServiceWorker from "./registerServiceWorker";
+import Counter from './components/counter';
 
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+registerServiceWorker();
